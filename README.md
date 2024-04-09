7ffba82bfd4389811864e35e44ed22e763108f8a
Beth <143623621+macbetthh@users.noreply.github.com>
2024-04-08 18:48:41 -0600

    random changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project_taskboard
  
+
+starter code
+https://github.com/coding-boot-camp/musical-happiness
--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,16 @@ function renderTaskList() {
 
 }
 
+//<div id="draggable" class="ui-widget-content">
+//<p>Drag me around</p>
+//</div>
+
+
+$( function() {
+    $( "#draggable" ).draggable();
+  } );
+
+
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
 
@@ -32,7 +42,53 @@ function handleDrop(event, ui) {
 
 }
 
+
+
+
+
+$( function() {
+
+    $( ".swim-lanes" ).sortable({
+        connectWith: ".swim-lanes",
+        handle: ".task-card-header",
+        cancel: ".task-card-toggle",
+        placeholder: "task-card-placeholder ui-corner-all"
+      });
+
+ 
+    $( ".task-card" )
+      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+      .find( ".task-card-header" )
+        .addClass( "ui-widget-header ui-corner-all" )
+        .prepend( "<span class='ui-icon ui-icon-minusthick task-card-toggle' style='margin-right: 15px'></span>");
+ 
+    $( ".task-card-toggle" ).on( "click", function() {
+      var icon = $( this );
+      icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+      icon.closest( ".task-card" ).find( ".task-card-content" ).toggle();
+    });
+  } );
+
+
+
+
+
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
 });
+
+
+
+
+
+$( function() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
+  } );
+
+
+
+  
